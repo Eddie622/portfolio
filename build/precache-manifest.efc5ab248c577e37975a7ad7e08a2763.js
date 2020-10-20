bfa19e0b6f69ae5bd09dc69ea15dd4b18d4bf118
heriberto.torres <eddietorres622@gmail.com>
2020-10-19 22:05:05 -0400

    initial

---
+++ build/precache-manifest.efc5ab248c577e37975a7ad7e08a2763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fa751340174ae7d4b7f99f03107717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ffc71fce4cd02e00dff",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "989ebaa290caff02536a",
+    "url": "/static/js/2.ca01c2d3.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.ca01c2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffc71fce4cd02e00dff",
+    "url": "/static/js/main.0ad299cc.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
---
+++ src/Components/Crystal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import test from '../og-1603080039600.png'
+
+function Crystal() {
+    return (
+        <div className="crystalDiv">
+            <img style={{objectFit: "contain", height: "900px", margin: "50px"}}src={test}/>
+        </div>
+    )
+}
+
+export default Crystal
